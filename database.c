41ede756a4d59b12cd6deca4f7d8e4979aeec567
Iburinoc <iburinoc@gmail.com>
2014-04-30 13:31:19 -0400

    There are only 2 hard problems in computer science: naming, cache invalidations, and off by one errors.

--- database.c
+++ database.c
@@ -185,7 +185,7 @@ int db_add_pw(pwdb_t* db, passw_t* pw) {
 		return ALLOC_FAIL;
 	}
 	
-	memmove(&db->pws[inspos+1], &db->pws[inspos], ((db->num + 1) - inspos) * sizeof(passw_t*));
+	memmove(&db->pws[inspos+1], &db->pws[inspos], (db->num - inspos) * sizeof(passw_t*));
 
 	/* success! */
 	db->pws[inspos] = pw;
